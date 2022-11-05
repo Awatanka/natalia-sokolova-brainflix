94d27ffbaefb7cc3ecbbf9240f7cc7785a303727
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-05 14:06:19 -0700

    finished with header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,20 +10,20 @@ export default function Header() {
       <div className="header__logo">
         <img src={Logo} alt="Logo" />
       </div>
+      <div className="header__block">
+        <div className="header__search">
+          <input placeholder="     Search" className="search__placeholder" />
+          <img src={Search} alt="Search" className="search__image" />
+        </div>
 
-      <div className="header__search">
-        <input placeholder="Search" className="header__search-placeholder" />
+        <button className="header__upload" href="#upload">
+          <img src={Upload} alt="arrowUp" className="header__upload-image" />
+          Upload
+        </button>
 
-        <img src={Search} alt="Search" className="header__search-image" />
-      </div>
-
-      <button className="header__upload" href="#upload">
-        <img src={Upload} alt="arrowUp" className="header__upload-image" />
-        Upload
-      </button>
-
-      <div className="header__avatar">
-        <img src={Avatar} alt="man" className="header__avatar-image" />
+        <div className="header__avatar">
+          <img src={Avatar} alt="man" className="header__avatar-image" />
+        </div>
       </div>
     </header>
   );
