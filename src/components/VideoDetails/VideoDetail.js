6c80e44a8ae4700e7e3c69c303796754131b50b6
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-14 07:56:27 -0800

    deleted utils and uncommented sections of code

--- src/components/VideoDetails/VideoDetail.js
+++ src/components/VideoDetails/VideoDetail.js
@@ -6,16 +6,13 @@ import axios from "axios";
 import SideBar from "../sideBar/Sidebar";
 import Comments from "../Comments/Comments";
 import Description from "../description/Description";
-// import VideoBanner from "../VideoBanner/VideoBanner";
 
 const URL = `https://project-2-api.herokuapp.com/videos/`;
-const API_KEY = `?api_key=70453c80-7523-4f69-a815-ba520ea2f155`;
-// const getVideoID = (videoId) => `${URL}&i=${videoId}`;
+const API_KEY = `?api_key=${process.env.REACT_APP_VIDEO_API_KEY}`;
 
 export default function VideoDetails(/*{ videoPlayer  }*/) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState();
-  // const [mainVideo, setMainVideo] = useState([0]);
 
   const params = useParams();
 
@@ -28,18 +25,9 @@ export default function VideoDetails(/*{ videoPlayer  }*/) {
         setVideos(response.data.filter((video) => video.id !== videoId));
         axios.get(URL + videoId + API_KEY).then((response) => {
           setSelectedVideo(response.data);
-          // setMainVideo(response.data[0]);
         });
-
-        // setVideos(response.data.filter((video) => video.id === videoId));
-        // axios.get(URL + videoId + API_KEY).then((response) => {
-        //   setMainVideo(response.data);
-        //   console.log(response.data);
-        // });
       })
-      // .then((videoId) => {
 
-      // })
       .catch((error) => console.log(error));
   }, [params]);
 
@@ -71,29 +59,6 @@ export default function VideoDetails(/*{ videoPlayer  }*/) {
           </section>
         </>
       )}
-
-      {/* {
-        <>
-          <div className="videoBanner">
-            <video
-              controls
-              className="videoBanner__video"
-              poster={mainVideo.image}
-            >
-              <source src="#" type="video/mp4" />
-            </video>
-          </div>
-          <section className="main">
-            <section className="main__left">
-              <Description videoPlayer={mainVideo} />
-              <Comments comments={mainVideo.comments} />
-            </section>
-            <section className="main__right">
-              <SideBar videos={videos} />
-            </section>
-          </section>
-        </>
-      } */}
     </>
   );
 }
--- src/components/sideBar/Sidebar.js
+++ src/components/sideBar/Sidebar.js
@@ -1,6 +1,4 @@
 import "./sideBar.scss";
-// import axios from "axios";
-// import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function SideBar({ videos }) {
